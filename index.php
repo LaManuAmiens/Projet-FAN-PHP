fb51df48b6eea0735f0eeff4fa3db00528b523cf
root <personne.nico@gmail.com>
2018-12-13 15:46:25 +0100

    partie nico

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+$fichier = "source.xml";  
+$xml = simplexml_load_file($fichier);
+echo $xml->page[0]->menu;
+echo $xml->page[0]->title;
+echo $xml->page[0]->content;
+?>
