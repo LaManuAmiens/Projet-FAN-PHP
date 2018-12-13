b63be81ed84c2522b212da5aaccf46ae3e577d8c
Fa <ngomfa@gmail.com>
2018-12-13 15:45:39 +0100

    page2

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+ 
+$xml=simplexml_load_file("source.xml") or die("Error: Cannot create object");
 
+    echo $xml->page[1]->menu;
+    echo $xml->page[1]->title;
+    echo $xml->page[1]->content;
+    
+
+?> 
